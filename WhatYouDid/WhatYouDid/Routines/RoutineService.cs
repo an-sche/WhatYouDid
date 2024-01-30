fa2a31626c75ab37506072c391964bf30de34e53
an-sche <bscheit92@gmail.com>
2024-01-30 01:38:59 -0500

    Updates

--- WhatYouDid/WhatYouDid/Routines/RoutineService.cs
+++ WhatYouDid/WhatYouDid/Routines/RoutineService.cs
@@ -39,6 +39,11 @@ public class RoutineService : IRoutineService
         return await _db.Routines.ToListAsync();
     }
 
+	public IQueryable<Routine> GetRoutinesByUserAsync(ApplicationUser user)
+	{
+		return _db.Routines.OrderBy(x => x.Name).Where(x => x.CreateUser == user);
+	}
+
 	public IQueryable<Routine> GetUserRoutinesQueryable(ApplicationUser user)
 	{
         return _db.Routines.OrderBy(x => x.Name).Where(x => x.CreateUser == user);
