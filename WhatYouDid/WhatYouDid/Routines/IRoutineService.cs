56c08dd4746505347ac69c46f75f7c6ca5cb7093
an-sche <bscheit92@gmail.com>
2024-03-10 23:32:45 -0400

    Adds workout page. Changes EditRoutine to CreateRoutine

--- WhatYouDid/WhatYouDid/Routines/IRoutineService.cs
+++ WhatYouDid/WhatYouDid/Routines/IRoutineService.cs
@@ -6,7 +6,6 @@ public interface IRoutineService
 {
     IQueryable<Routine> GetUserRoutinesQueryable(ApplicationUser user);
     Task<List<Routine>> GetRoutinesAsync();
-    IQueryable<Routine> GetRoutinesByUserAsync(ApplicationUser user);
     Task<Routine?> GetRoutineAsync(int routineId);
     Task<Routine> AddRoutineAsync(Routine routine);
     Task<Routine> UpdateRoutineAsync(Routine routine);
