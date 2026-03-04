11c82f6099a062c30d31c29857f8221bead2ec8f
an-sche <bscheit92@gmail.com>
2026-03-04 00:41:10 -0500

    Keep screen awake during timer

---
+++ WhatYouDid/WhatYouDid/wwwroot/wakelock.js
@@ -0,0 +1,22 @@
+﻿// Use wakelock to prevent the users screen from going to sleep
+// For Example: When the timer is running, it shouldn't go to sleep
+
+let wakeLock = null;
+
+window.wakeLockHelper = {
+    enable: async () => {
+        try {
+            if ('wakeLock' in navigator) {
+                wakeLock = await navigator.wakeLock.request('screen');
+            }
+        } catch { }
+    },
+    disable: async () => {
+        try {
+            if (wakeLock !== null) {
+                await wakeLock.release();
+                wakeLock = null;
+            }
+        } catch { }
+    }
+};
