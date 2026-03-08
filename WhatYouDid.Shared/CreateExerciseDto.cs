64cc41902920922ca0df05ede870e39275163363
an-sche <bscheit92@gmail.com>
2026-03-08 12:30:40 -0400

    Update Dto file names

--- WhatYouDid.Shared/CreateExerciseDto.cs
+++ WhatYouDid.Shared/CreateExerciseDto.cs
@@ -2,13 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WhatYouDid.Shared;
 
-public class CreateRoutineDto
-{
-    [Required, MaxLength(50), MinLength(3)]
-    public required string Name { get; set; }
-    public required List<CreateExerciseDto> Exercises { get; set; }
-}
-
 public class CreateExerciseDto
 {
     [Required, MaxLength(50), MinLength(3)]
