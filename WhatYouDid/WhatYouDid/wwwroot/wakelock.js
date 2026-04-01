b9188197abb276ca17e0810726409e01f326668b
an-sche <bscheit92@gmail.com>
2026-04-01 14:43:50 -0400

    wakelock fix

--- WhatYouDid/WhatYouDid/wwwroot/wakelock.js
+++ WhatYouDid/WhatYouDid/wwwroot/wakelock.js
@@ -6,7 +6,7 @@ let wakeLock = null;
 window.wakeLockHelper = {
     enable: async () => {
         try {
-            if ('wakeLock' in navigator && 'request' in navigate.wakeLock) {
+            if ('wakeLock' in navigator && 'request' in navigator.wakeLock) {
                 wakeLock = await navigator.wakeLock.request('screen');
             }
         } catch { }
